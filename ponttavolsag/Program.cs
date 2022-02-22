94660f951f409b88e86f0054937ef125449b9dc5
Tamás Kaprinyák <2021295@dszcberegszaszi.hu>
2022-02-22 09:48:51 +0100

    pontszam

---
+++ ponttavolsag/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ponttavolsag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x1, x2, y1, y2;
+
+            Console.WriteLine("Add meg az x1-et:");
+            x1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Add meg az x2-et:");
+            x2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Add meg az y1-et:");
+            y1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Add meg az y2-et:");
+            y2 = float.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine(Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)));
+
+            Console.ReadKey();
+        }
+    }
+}
