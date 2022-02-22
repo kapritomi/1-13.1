7799dc97f116a3efc2130b40e0c577cbb33150d6
Tamás Kaprinyák <2021295@dszcberegszaszi.hu>
2022-02-22 09:36:23 +0100

    testtomeg

--- testtomeg/Program.cs
+++ testtomeg/Program.cs
@@ -10,6 +10,21 @@ namespace testtomeg
     {
         static void Main(string[] args)
         {
+            double suly, bmi;
+            int magassag;
+
+            Console.WriteLine("Testsúly");
+            suly = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Magasság");
+            magassag = Convert.ToInt32(Console.ReadLine());
+
+            bmi = Math.Round(suly / (magassag * magassag)*10000,2);
+
+            Console.WriteLine(bmi);
+
+
+            Console.ReadKey();
         }
     }
 }
