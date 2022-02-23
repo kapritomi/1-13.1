68aff1e4f646766fc80a791c38d57d8ed1eb8d64
Tamás Kaprinyák <2021295@dszcberegszaszi.hu>
2022-02-23 09:47:48 +0100

    született

---
+++ 1. feladat/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.feladat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < ; i++)
+            {
+
+            }
+        }
+    }
+}
