e5ab3f20e795881173849fabda9ebdc6245950fd
Tamás Kaprinyák <2021295@dszcberegszaszi.hu>
2022-02-23 09:08:22 +0100

    konyves

---
+++ konyves/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konyves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cim;
+            int oldal;
+
+            Console.WriteLine("Add meg a könyv címét");
+            cim = Console.ReadLine();
+
+            Console.WriteLine("Add meg a könyv oldalainak számát");
+            oldal = Convert.ToInt32(Console.ReadLine());
+
+
+            if(oldal > 150)
+            {
+                Console.WriteLine("A {0} rövid terjedelmű könyv.",cim);
+            }
+            else if(oldal < 150)
+            {
+                Console.WriteLine("A {0} hosszú terjedelmű könyv.", cim);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
