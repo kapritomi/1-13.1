bced29103be91e64b9876c9c66a906c79600c2cf
Tamás Kaprinyák <2021295@dszcberegszaszi.hu>
2022-02-23 08:56:59 +0100

    konyv

---
+++ konyvek/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konyvek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int elso, masodik;
+
+            Console.WriteLine("Add meg az első számot:");
+            elso = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Add meg a második számot:");
+            masodik = Convert.ToInt32(Console.ReadLine());
+
+            if (elso < 0 && masodik < 0)
+            {
+                Console.WriteLine("Mindkét szám negatív");
+            }
+            else if (elso < 0)
+            {
+                Console.WriteLine("A két szám közül az első negatív");
+            }
+            else if(masodik < 0)
+            {
+                Console.WriteLine("A két szám közül a második negatív");
+            }
+            else
+            {
+                Console.WriteLine("A két szám közül egyik sem negatív");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
