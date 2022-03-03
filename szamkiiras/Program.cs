0d415cbdf28a8eaff946834a8964f90380e6289b
Tamás Kaprinyák <2021295@dszcberegszaszi.hu>
2022-03-03 10:00:11 +0100

    asd

---
+++ szamkiiras/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szamkiiras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam = 0;
+
+
+        }
+    }
+}
---
+++ valto/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace valto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam = 0;
+            
+
+            Console.WriteLine("Add meg az összeget:");
+            szam = Convert.ToInt32(Console.ReadLine());
+
+            Console.ReadKey();
+        }
+    }
+}
