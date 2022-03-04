b7f88f9e17439099359b26d0447c1eb68f6e0587
Tamás Kaprinyák <2021295@dszcberegszaszi.hu>
2022-03-04 09:04:29 +0100

    kor

---
+++ kor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kor
+{
+
+    class Kor
+    {
+        double sugar;
+        public Kor()
+        {
+
+        }
+        public double teruletszamolo()
+        {
+            return this.sugar * this.sugar * 3.14;
+        }
+        public double keruletszamolo()
+        {
+            return 2 * 3.14 * this.sugar;
+        }
+       
+
+        public void szamolo()
+        { 
+           this.sugar = Convert.ToDouble(Console.ReadLine());
+
+        }
+    }
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kor k = new Kor();
+            Console.WriteLine("Add meg a sugarat:");
+            k.szamolo();
+            Console.WriteLine("A kör területe: {0}, A kerülete: {1}", k.teruletszamolo(),k.keruletszamolo());
+
+            Console.ReadKey();
+        }
+    }
+}
