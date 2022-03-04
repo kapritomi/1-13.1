654d68c3e2348606bcb98b94e42daca6fc2404e3
Tamás Kaprinyák <2021295@dszcberegszaszi.hu>
2022-03-04 11:40:10 +0100

    asdas

--- kor/Program.cs
+++ kor/Program.cs
@@ -9,7 +9,7 @@ namespace kor
 
     class Kor
     {
-        double sugar;
+        private double sugar;
         public Kor()
         {
 
@@ -22,25 +22,45 @@ namespace kor
         {
             return 2 * 3.14 * this.sugar;
         }
-       
 
         public void szamolo()
-        { 
-           this.sugar = Convert.ToDouble(Console.ReadLine());
-
+        {
+            this.sugar = Convert.ToDouble(Console.ReadLine());
         }
     }
+
+    class Henger : Kor
+    {
+        private double magassag;
+
+        public Henger()
+        {
+            
+        } 
+        public void magassagszamolo()
+        {
+            this.magassag = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public double terfogat()
+        {
+            return this.teruletszamolo() *  this.magassag;
+        }
+    }
+
 
     
     class Program
     {
         static void Main(string[] args)
         {
-            Kor k = new Kor();
-            Console.WriteLine("Add meg a sugarat:");
-            k.szamolo();
-            Console.WriteLine("A kör területe: {0}, A kerülete: {1}", k.teruletszamolo(),k.keruletszamolo());
+            Henger a = new Henger();
 
+            Console.WriteLine("Add meg a sugarat:");
+            a.szamolo();
+            Console.WriteLine("A kör területe: {0}, A kerülete: {1}", a.teruletszamolo(),a.keruletszamolo());
+            a.magassagszamolo();
+            Console.WriteLine("A henger térfogata:{0} Alapterülete: {1}",a.terfogat(),a.teruletszamolo());
             Console.ReadKey();
         }
     }
