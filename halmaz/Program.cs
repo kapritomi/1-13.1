9698bc8db86800a89285894b310269019c5ee18a
Kaprinyák Tamás <2021295@bp.local>
2022-03-18 09:46:16 +0100

    asd

--- halmaz/Program.cs
+++ halmaz/Program.cs
@@ -30,19 +30,80 @@ namespace halmaz
 
             for (int i = 0; i < a.Length; i++)
             {
-                a[i] = rnd.Next(-100, 101);
-                Console.WriteLine(a[i]);
+                a[i] = rnd.Next(-100, 100);
+                Console.WriteLine("a tomb: {0}", a[i]);
             }
 
             for (int i = 0; i < b.Length; i++)
             {
-                b[i] = rnd.Next(-100, 101);
-                Console.WriteLine(b[i]);
+                b[i] = rnd.Next(-100, 100);
+                Console.WriteLine("b tomb: {0}",b[i]);
             }
 
+            Console.WriteLine("a-b");
+            int counter=0;
+            List<int> nemismetlodo = new List<int>();
+
+                for (int i = 0; i < a.Length; i++)
+			    {
+                    for (int j = 0; j < b.Length; j++)
+			        {
+                        if(a[i] != b[j])
+                        {
+                            counter++;
+                        }
+			        }
+                    if(counter == a.Length)
+                    {
+                        nemismetlodo.Add(a[i]);
+
+                    }
+                    counter = 0;
+			    }
+                foreach (var item in nemismetlodo)
+	            {
+                    Console.WriteLine(item);
+	            }
             
+
+            Console.WriteLine("b-a");
+            int counter1=0;
+            List<int> nemismetlodo1 = new List<int>();
+
+                for (int i = 0; i < a.Length; i++)
+			    {
+                    for (int j = 0; j < b.Length; j++)
+			        {
+                        if(b[i] != a[j])
+                        {
+                            counter1++;
+                        }
+			        }
+                    if(counter1 == a.Length)
+                    {
+                        nemismetlodo1.Add(a[i]);
+
+                    }
+                    counter1 = 0;
+			    }
+                foreach (var item in nemismetlodo1)
+	            {
+                    Console.WriteLine(item);
+	            }
+
+                for (int i = 0; i < a.Length; i++)
+			    {
+                    for (int j = 0; j < b.Length; j++)
+			        {
+                        if(a[i] == b[j])
+                        {
+                            Console.WriteLine();
+                        }
+			        }
+			    }
+
+
         }
-     
        
     }
     class Program
