e853612a96d8d2d7404c6deb2ff564c764c708fc
Tamás Kaprinyák <2021295@dszcberegszaszi.hu>
2022-03-22 09:10:24 +0100

    asd

---
+++ homerseklet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homerseklet
+{
+    class Homerseklet
+    {
+        Random rnd = new Random();
+
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> numberNames = new Dictionary<string, int> {
+                {"Január", 1 },{"Február", 2},{"Március", 3},{"Április", 4},{"Május", 5},{"Június", 6},{"Július", 7},{"Augusztus",8}
+            
+            };
+        }
+    }
+}
