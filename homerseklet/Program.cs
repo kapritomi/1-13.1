37038638a27b127110d929f40420022d79dea3b9
Tamás Kaprinyák <2021295@dszcberegszaszi.hu>
2022-03-22 12:30:13 +0100

    asd

--- homerseklet/Program.cs
+++ homerseklet/Program.cs
@@ -6,24 +6,41 @@ using System.Threading.Tasks;
 
 namespace homerseklet
 {
-    class Homerseklet
-    {
-        Random rnd = new Random();
-
-
-
-    }
+   
 
     class Program
     {
         static void Main(string[] args)
         {
-            Dictionary<string, int> numberNames = new Dictionary<string, int> {
+            Random rnd = new Random();
+            int[,] a = new int[30,4];
+            string honap;
+
+            Dictionary<string, int> honapok = new Dictionary<string, int> {
                 {"Január", 1 },{"Február", 2},{"Március", 3},
                 {"Április", 4},{"Május", 5},{"Június", 6},
                 {"Július", 7},{"Augusztus",8},{"Szeptember",9},
                 {"Október",10},{"November",11},{"December",12}
             };
+
+            honap = Console.ReadLine();
+            //Tavasz
+            if(honapok[honap] >= 3 && honapok[honap] <= 5)
+            {
+                for (int i = 0; i < a.GetLength(0); i++)
+			    {
+                    for (int j = 0; j < a.GetLength(1); j++)
+			        {
+                        a[i,j] = rnd.Next(-10,20);
+                        Console.WriteLine(a[i,j]);
+			        }
+                    
+                    
+			    }
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
