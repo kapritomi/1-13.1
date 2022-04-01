6effd9fc74438918395bb7d00dc79967c9b1ee43
Tamás Kaprinyák <2021295@dszcberegszaszi.hu>
2022-04-01 09:59:02 +0200

    asd

---
+++ Lotto/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lotto
+{
+    class Lotto
+    {
+        Random rnd = new Random();
+        int[] szamok = new int[5];
+        public void szam()
+        {
+            for (int i = 0; i < this.szamok.Length; i++)
+            {
+                this.szamok[i] = rnd.Next(1,90);
+                Console.WriteLine(szamok[i]);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lotto l = new Lotto();
+
+            l.szam();
+
+            Console.ReadKey();
+        }
+    }
+}
