1b0b5010934bf9ece94d654fcbbd3a66a0a08040
Tamás Kaprinyák <2021295@dszcberegszaszi.hu>
2022-04-04 13:15:31 +0200

    asd

---
+++ Elágazás/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elágazás
+{
+    class Elagazas
+    {
+        Random rnd = new Random();
+        List<int> parts = new List<int>();
+        int hossz;
+
+        public void feltolt()
+        {
+            this.hossz = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < this.hossz; i++)
+            {
+                this.parts.Add(rnd.Next(10, 100));
+            }
+        }
+        public void asd()
+        {
+            foreach (var item in this.parts)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void vizsgal()
+        {
+            foreach (var item in this.parts)
+            {
+                if(item % 2 == 0)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elagazas e = new Elagazas();
+            e.feltolt();
+            e.asd();
+            Console.WriteLine();
+            e.vizsgal();
+
+            Console.ReadKey();
+
+        }
+    }
+}
