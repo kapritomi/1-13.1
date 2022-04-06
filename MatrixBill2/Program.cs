608a6e4cb4ef6af33c8431dab42f966a48537ccd
Tamás Kaprinyák <2021295@dszcberegszaszi.hu>
2022-04-06 10:32:56 +0200

    asd

--- MatrixBill2/Program.cs
+++ MatrixBill2/Program.cs
@@ -20,44 +20,47 @@ namespace MatrixBill2
         public void feltolt()
         {
             this.tomb = new int[this.a, this.b];
-
             for (int i = 0; i < this.tomb.GetLength(0); i++)
             {
                 for (int j = 0; j < this.tomb.GetLength(1); j++)
                 {
-                    Console.WriteLine("Index: {0}", i);
-                    this.tomb[i, j] = Convert.ToInt32(Console.ReadLine()); 
+                    this.tomb[i, j] = Convert.ToInt32(Console.ReadLine());
+                    if (this.tomb.GetLength(1) == j+1)
+                    {
+                        this.sor(i);
+                    }
                 }
             }
         }
-
         public void kiiras()
         {
-
             for (int i = 0; i < this.tomb.GetLength(0); i++)
             {
                 Console.WriteLine();
                 for (int j = 0; j < this.tomb.GetLength(1); j++)
                 {
-                    Console.Write(this.tomb[i, j]);
+                    Console.Write("{0} ",this.tomb[i, j]);
                 }
             }
         }
-
+        public void sor(int y)
+        {
+                for (int i = 0; i < this.tomb.GetLength(1); i++)
+                {
+                    Console.Write("{0} ", this.tomb[y, i]);
+                }
+            Console.WriteLine();
+        }
     }
-
 
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Sorok/Oszlopok száma:");
-
             Matrix2 m = new Matrix2(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
-
             m.feltolt();
             m.kiiras();
-
 
             Console.ReadKey();
         }
