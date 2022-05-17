f39a2af2aba155489b6f8bc40e7baddf7686c3de
Tamás Kaprinyák <2021295@dszcberegszaszi.hu>
2022-05-17 11:40:09 +0200

    asd

---
+++ CsempeTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using csempe;
+
+namespace CsempeTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            Csempe c = new Csempe();
+
+            double vartEredm = 687.5;
+            double kapottEredm = c.Eredm();
+
+            Assert.AreEqual(kapottEredm, vartEredm);
+        }
+    }
+}
---
+++ csempe/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csempe
+{
+    public class Csempe
+    {
+        double szel;
+        double mag;
+        double csempe = 0.2 * 0.2;
+        double terulet;
+        public Csempe() {
+            this.szel = 5;
+            this.mag = 5;
+        }
+        public void setSzel()
+        {
+            this.szel = double.Parse(Console.ReadLine());
+        }
+        public void setMag()
+        {
+            this.mag = double.Parse(Console.ReadLine());
+        }
+        public double Eredm()
+        {
+            this.terulet = this.szel * this.mag / this.csempe;
+            return Math.Round(terulet + terulet * 0.1,2);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Csempe c = new Csempe();
+            Console.WriteLine(c.Eredm());
+
+            Console.ReadKey();
+        }
+    }
+}
