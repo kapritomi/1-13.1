4619d20f355c1fe909632214dffabe30359aacdb
Tamás Kaprinyák <2021295@dszcberegszaszi.hu>
2022-05-18 10:34:32 +0200

    asd

---
+++ Autók/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Autók
+{
+    internal class Autok
+    {
+        private class Autolista
+        {
+            private string rendszam;
+            private string gyart;
+            private string tipus;
+            private DateTime gyartas;
+            private DateTime muszaki;
+            private string tulaj;
+
+            public Autolista() { }
+
+            public Autolista(string rendszam, string gyart, string tipus, DateTime gyartas, DateTime muszaki, string tulaj)
+            {
+                this.rendszam = rendszam;
+                this.gyart = gyart;
+                this.tipus = tipus;
+                this.gyartas = gyartas;
+                this.muszaki = muszaki;
+                this.tulaj = tulaj;
+            }
+
+            public void printAuto()
+            {
+                Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}", rendszam, gyart, tipus, gyartas, muszaki, tulaj);
+            }
+
+            private List<Autolista> Kocsilista;
+
+            private string fajlNev;
+
+            public Autok()
+            {
+                Kocsilista = new List<Autolista>();
+            }
+
+            public void Beolvas()
+            {
+                string sor;
+                string[] mezok;
+
+                Console.WriteLine("Adja meg a szövegfájl nevét elérési úttal!");
+
+                fajlNev = Console.ReadLine();
+                if (!fajlNev.EndsWith(".txt")) fajlNev += ".txt";
+
+                using (StreamReader sr = new StreamReader(fajlNev, Encoding.Default))
+                    while (sr.EndOfStream == false)
+                    {
+                        sor = sr.ReadLine();
+                        if (sor.Trim().Length == 0) continue;
+
+                        mezok = sor.Split(',');
+
+                        Kocsilista.Add(new Autolista(
+                            mezok[0],
+                            mezok[1],
+                            mezok[2],
+                            DateTime.Parse(mezok[3]),
+                            DateTime.Parse(mezok[4]),
+                            mezok[3]));
+                    }
+            }
+
+            public void Kiir()
+            {
+                foreach (Autolista p in Kocsilista)
+                {
+                    p.printAuto();
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Autok a = new Autok();
+            a.Beolvas();
+            a.Kiir();
+        }
+    }
+}
---
+++ Rekordok/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Rekordok
+{
+    internal class Baratlista
+    {
+        private class Barat
+        {
+            private string nev;
+            private DateTime szuletett;
+            private char neme;
+            private int bulis;
+
+            public Barat() { }
+
+            public Barat(string nev, DateTime szuletett, char neme, int bulis)
+            {
+                this.nev = nev;
+                this.szuletett = szuletett;
+                this.neme = neme;
+                this.bulis = bulis;
+            }
+
+            public void printBarat()
+            {
+                Console.WriteLine("{0,-20} {1,10} {2,1} {3}", nev, szuletett.ToShortDateString(), neme, bulis);
+            }
+
+            private List<Barat> baratLista;
+
+            private string fajlNev;
+            public Baratlista()
+            {
+                baratLista = new List<Barat>();
+            }
+            public void Beolvas() 
+            {
+                string sor;
+                string[] mezok;
+
+                Console.WriteLine("Adja meg a szövegfájl nevét elérési úttal!");
+
+                fajlNev = Console.ReadLine();
+                if (!fajlNev.EndsWith(".txt")) fajlNev += ".txt";
+
+                using (StreamReader sr = new StreamReader(fajlNev, Encoding.Default))
+                    while(sr.EndOfStream == false)
+                    {
+                        sor = sr.ReadLine();
+                        if (sor.Trim().Length == 0) continue;
+
+                        mezok = sor.Split(',');
+
+                        baratLista.Add(new Barat(
+                            mezok[0],
+                            DateTime.Parse(mezok[1]),
+                            char.Parse(mezok[2]),
+                            int.Parse(mezok[3])));
+                    }
+            }
+
+            public void Kiir()
+            {
+                foreach(Barat p in baratLista)
+                {
+                    p.printBarat();
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Baratlista bl = new Baratlista();
+            bl.Beolvas();
+            bl.Kiir();
+        }
+    }
+}
