f302c84ca5459eed8871c62a1c6754450e98c1c5
Tamás Kaprinyák <2021295@dszcberegszaszi.hu>
2022-05-26 10:01:04 +0200

    asd

---
+++ labelszoveg/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labelszoveg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblfull.Text = "Kattints a képernyőn valahova";
+        }
+        private void lblfull_MouseClick(object sender, MouseEventArgs e)
+        {
+            int o = (int)e.X / (lblfull.Width / 3);
+            int s = (int)e.Y / (lblfull.Height / 3);
+        }
+    }
+}
