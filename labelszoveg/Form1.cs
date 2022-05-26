57611ce9b429fb8b4307a9579ac3e7aca333b547
Tamás Kaprinyák <2021295@dszcberegszaszi.hu>
2022-05-26 10:50:09 +0200

    asd

--- labelszoveg/Form1.cs
+++ labelszoveg/Form1.cs
@@ -25,6 +25,20 @@ namespace labelszoveg
         {
             int o = (int)e.X / (lblfull.Width / 3);
             int s = (int)e.Y / (lblfull.Height / 3);
+
+            switch(s *3 + o)
+            {
+                case 0: lblfull.TextAlign = ContentAlignment.TopLeft; break;
+                case 1: lblfull.TextAlign = ContentAlignment.TopCenter; break;
+                case 2: lblfull.TextAlign = ContentAlignment.TopRight; break;
+                case 3: lblfull.TextAlign = ContentAlignment.MiddleLeft; break;
+                case 4: lblfull.TextAlign = ContentAlignment.MiddleCenter; break;
+                case 5: lblfull.TextAlign = ContentAlignment.MiddleRight; break;
+                case 6: lblfull.TextAlign = ContentAlignment.BottomLeft; break;
+                case 7: lblfull.TextAlign = ContentAlignment.BottomCenter; break;
+                case 8: lblfull.TextAlign = ContentAlignment.BottomRight; break;
+                
+            }
         }
     }
 }
