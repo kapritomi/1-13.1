27efd55f35a242de844e2253f7f95fb93140e3d2
Tamás Kaprinyák <2021295@dszcberegszaszi.hu>
2022-05-27 10:49:19 +0200

    asd

---
+++ kalkulátor/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kalkulátor
+{
+    public class Kalkulator
+    {
+        double szam1;
+        double szam2;
+        char muv;
+        double eredm;
+
+        public Kalkulator() { }
+
+        public void beker()
+        {
+            Console.WriteLine("Add meg a számot:");
+            szam1 = 10.0;
+
+            Console.WriteLine("Add meg a másik számot:");
+            szam2 = 10.0;
+
+            Console.WriteLine("Add meg a műveletjelet:");
+            muv = '-';
+        }
+
+        public double muvelet()
+        {
+            if(this.muv == '+')
+            {
+                eredm =  szam1 + szam2;
+            }
+            if(this.muv == '-')
+            {
+                eredm = szam1 - szam2;
+            }
+            if(this.muv == '*')
+            {
+                eredm = szam1 * szam2;
+            }
+            if(this.muv == '/')
+            {
+                eredm = szam1 / szam2;
+            }
+            return eredm;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
