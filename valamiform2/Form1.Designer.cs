474c15458d3f756b053fc5784fa04af5dd473314
Tamás Kaprinyák <2021295@dszcberegszaszi.hu>
2022-05-30 12:49:30 +0200

    asd

---
+++ valamiform2/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿
+namespace valamiform2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.kozep = new System.Windows.Forms.Button();
+            this.fel = new System.Windows.Forms.Button();
+            this.felulre = new System.Windows.Forms.Button();
+            this.le = new System.Windows.Forms.Button();
+            this.lefele = new System.Windows.Forms.Button();
+            this.jobbra = new System.Windows.Forms.Button();
+            this.jobbszel = new System.Windows.Forms.Button();
+            this.balra = new System.Windows.Forms.Button();
+            this.balszel = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.oppno = new System.Windows.Forms.Button();
+            this.oppcsok = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.oppcsok1 = new System.Windows.Forms.Button();
+            this.oppno1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // kozep
+            // 
+            this.kozep.Location = new System.Drawing.Point(327, 230);
+            this.kozep.Name = "kozep";
+            this.kozep.Size = new System.Drawing.Size(75, 23);
+            this.kozep.TabIndex = 0;
+            this.kozep.Text = "Középre";
+            this.kozep.UseVisualStyleBackColor = true;
+            // 
+            // fel
+            // 
+            this.fel.Location = new System.Drawing.Point(327, 184);
+            this.fel.Name = "fel";
+            this.fel.Size = new System.Drawing.Size(75, 23);
+            this.fel.TabIndex = 1;
+            this.fel.Text = "Fel";
+            this.fel.UseVisualStyleBackColor = true;
+            // 
+            // felulre
+            // 
+            this.felulre.Location = new System.Drawing.Point(327, 140);
+            this.felulre.Name = "felulre";
+            this.felulre.Size = new System.Drawing.Size(75, 23);
+            this.felulre.TabIndex = 2;
+            this.felulre.Text = "Felülre";
+            this.felulre.UseVisualStyleBackColor = true;
+            // 
+            // le
+            // 
+            this.le.Location = new System.Drawing.Point(327, 272);
+            this.le.Name = "le";
+            this.le.Size = new System.Drawing.Size(75, 23);
+            this.le.TabIndex = 3;
+            this.le.Text = "Le";
+            this.le.UseVisualStyleBackColor = true;
+            // 
+            // lefele
+            // 
+            this.lefele.Location = new System.Drawing.Point(327, 317);
+            this.lefele.Name = "lefele";
+            this.lefele.Size = new System.Drawing.Size(75, 23);
+            this.lefele.TabIndex = 4;
+            this.lefele.Text = "Lefele";
+            this.lefele.UseVisualStyleBackColor = true;
+            // 
+            // jobbra
+            // 
+            this.jobbra.Location = new System.Drawing.Point(408, 230);
+            this.jobbra.Name = "jobbra";
+            this.jobbra.Size = new System.Drawing.Size(75, 23);
+            this.jobbra.TabIndex = 5;
+            this.jobbra.Text = "Jobbra";
+            this.jobbra.UseVisualStyleBackColor = true;
+            // 
+            // jobbszel
+            // 
+            this.jobbszel.Location = new System.Drawing.Point(489, 230);
+            this.jobbszel.Name = "jobbszel";
+            this.jobbszel.Size = new System.Drawing.Size(75, 23);
+            this.jobbszel.TabIndex = 6;
+            this.jobbszel.Text = "Jobb Szél";
+            this.jobbszel.UseVisualStyleBackColor = true;
+            // 
+            // balra
+            // 
+            this.balra.Location = new System.Drawing.Point(246, 230);
+            this.balra.Name = "balra";
+            this.balra.Size = new System.Drawing.Size(75, 23);
+            this.balra.TabIndex = 7;
+            this.balra.Text = "Balra";
+            this.balra.UseVisualStyleBackColor = true;
+            // 
+            // balszel
+            // 
+            this.balszel.Location = new System.Drawing.Point(165, 230);
+            this.balszel.Name = "balszel";
+            this.balszel.Size = new System.Drawing.Size(75, 23);
+            this.balszel.TabIndex = 8;
+            this.balszel.Text = "Bal Szél";
+            this.balszel.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Coral;
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.oppcsok);
+            this.groupBox1.Controls.Add(this.oppno);
+            this.groupBox1.Location = new System.Drawing.Point(166, 366);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(399, 72);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // oppno
+            // 
+            this.oppno.Location = new System.Drawing.Point(81, 19);
+            this.oppno.Name = "oppno";
+            this.oppno.Size = new System.Drawing.Size(75, 23);
+            this.oppno.TabIndex = 5;
+            this.oppno.Text = "Növekszik";
+            this.oppno.UseVisualStyleBackColor = true;
+            // 
+            // oppcsok
+            // 
+            this.oppcsok.Location = new System.Drawing.Point(243, 19);
+            this.oppcsok.Name = "oppcsok";
+            this.oppcsok.Size = new System.Drawing.Size(75, 23);
+            this.oppcsok.TabIndex = 6;
+            this.oppcsok.Text = "Csökken";
+            this.oppcsok.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Coral;
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.oppcsok1);
+            this.groupBox2.Controls.Add(this.oppno1);
+            this.groupBox2.Location = new System.Drawing.Point(166, 8);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(399, 72);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // oppcsok1
+            // 
+            this.oppcsok1.Location = new System.Drawing.Point(243, 19);
+            this.oppcsok1.Name = "oppcsok1";
+            this.oppcsok1.Size = new System.Drawing.Size(75, 23);
+            this.oppcsok1.TabIndex = 6;
+            this.oppcsok1.Text = "Csökken";
+            this.oppcsok1.UseVisualStyleBackColor = true;
+            // 
+            // oppno1
+            // 
+            this.oppno1.Location = new System.Drawing.Point(81, 19);
+            this.oppno1.Name = "oppno1";
+            this.oppno1.Size = new System.Drawing.Size(75, 23);
+            this.oppno1.TabIndex = 5;
+            this.oppno1.Text = "Növekszik";
+            this.oppno1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(14, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Méret";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(6, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 24);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "átlá có";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.balszel);
+            this.Controls.Add(this.balra);
+            this.Controls.Add(this.jobbszel);
+            this.Controls.Add(this.jobbra);
+            this.Controls.Add(this.lefele);
+            this.Controls.Add(this.le);
+            this.Controls.Add(this.felulre);
+            this.Controls.Add(this.fel);
+            this.Controls.Add(this.kozep);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button kozep;
+        private System.Windows.Forms.Button fel;
+        private System.Windows.Forms.Button felulre;
+        private System.Windows.Forms.Button le;
+        private System.Windows.Forms.Button lefele;
+        private System.Windows.Forms.Button jobbra;
+        private System.Windows.Forms.Button jobbszel;
+        private System.Windows.Forms.Button balra;
+        private System.Windows.Forms.Button balszel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button oppcsok;
+        private System.Windows.Forms.Button oppno;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button oppcsok1;
+        private System.Windows.Forms.Button oppno1;
+    }
+}
+
