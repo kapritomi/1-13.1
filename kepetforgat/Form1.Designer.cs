fd837737fc74ac72d1cf89591dd444f8d0d59924
Tamás Kaprinyák <2021295@dszcberegszaszi.hu>
2022-06-01 09:47:33 +0200

    asd

---
+++ kepetforgat/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace kepetforgat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label11
+            // 
+            this.label11.Image = global::kepetforgat.Properties.Resources.Képkivágás5;
+            this.label11.Location = new System.Drawing.Point(470, 272);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(96, 90);
+            this.label11.TabIndex = 35;
+            // 
+            // label10
+            // 
+            this.label10.Image = global::kepetforgat.Properties.Resources.Képkivágás6;
+            this.label10.Location = new System.Drawing.Point(353, 317);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(102, 102);
+            this.label10.TabIndex = 34;
+            // 
+            // label9
+            // 
+            this.label9.Image = global::kepetforgat.Properties.Resources.Képkivágás7;
+            this.label9.Location = new System.Drawing.Point(240, 272);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(103, 90);
+            this.label9.TabIndex = 33;
+            // 
+            // label8
+            // 
+            this.label8.Image = global::kepetforgat.Properties.Resources.Képkivágás1;
+            this.label8.Location = new System.Drawing.Point(241, 53);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(102, 96);
+            this.label8.TabIndex = 32;
+            // 
+            // label6
+            // 
+            this.label6.Image = global::kepetforgat.Properties.Resources.Képkivágás2;
+            this.label6.Location = new System.Drawing.Point(364, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 90);
+            this.label6.TabIndex = 31;
+            // 
+            // label5
+            // 
+            this.label5.Image = global::kepetforgat.Properties.Resources.Képkivágás3;
+            this.label5.Location = new System.Drawing.Point(470, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 96);
+            this.label5.TabIndex = 30;
+            // 
+            // label4
+            // 
+            this.label4.Image = global::kepetforgat.Properties.Resources.Képkivágás4;
+            this.label4.Location = new System.Drawing.Point(532, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 101);
+            this.label4.TabIndex = 29;
+            // 
+            // label7
+            // 
+            this.label7.Image = global::kepetforgat.Properties.Resources.Képkivágás;
+            this.label7.Location = new System.Drawing.Point(190, 158);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(113, 101);
+            this.label7.TabIndex = 27;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::kepetforgat.Properties.Resources.balvissza;
+            this.button1.Location = new System.Drawing.Point(89, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 74);
+            this.button1.TabIndex = 36;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = global::kepetforgat.Properties.Resources.jobbvissza;
+            this.button2.Location = new System.Drawing.Point(671, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(55, 79);
+            this.button2.TabIndex = 37;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ kepetforgat/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kepetforgat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Image s = label7.Image;
+            label7.Image = label8.Image;
+            label8.Image = label6.Image;
+            label6.Image = label5.Image;
+            label5.Image = label4.Image;
+            label4.Image = label11.Image;
+            label11.Image = label10.Image;
+            label10.Image = label9.Image;
+            label9.Image = s;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Image s = label7.Image;
+            label7.Image = label9.Image;
+            label9.Image = label10.Image;
+            label10.Image = label11.Image;
+            label11.Image = label4.Image;
+            label4.Image = label5.Image;
+            label5.Image = label6.Image;
+            label6.Image = label8.Image;
+            label8.Image = s;
+        }
+    }
+}
